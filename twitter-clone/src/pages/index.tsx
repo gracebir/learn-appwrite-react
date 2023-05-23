1d1c8b659f4b60f8ddff19ccd7bb9bfb5ba2a4f1
gracebir <grace.devolop@gmail.com>
2023-05-23 12:16:17 +0200

    create new document entry

--- twitter-clone/src/pages/index.tsx
+++ twitter-clone/src/pages/index.tsx
@@ -1,120 +1,125 @@
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
-import { Client, Databases, Models} from 'appwrite'
+import { Client, Databases, Models, Account} from 'appwrite'
 import type { InferGetServerSidePropsType, GetServerSideProps } from 'next';
+import { useEffect, useState } from 'react';
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home({ tweets }: InferGetServerSidePropsType<typeof getServerSideProps>)  {
   console.log("collection",tweets)
+  const [user, setUser] = useState("")
+
+  useEffect(()=> {
+    const client = new Client();
+    const account = new Account(client);
+
+    client
+    .setEndpoint(process.env.NEXT_PUBLIC_ENDPOINT!)
+    .setProject(process.env.NEXT_PUBLIC_PROJECT!)
+
+    const promise = account.get()
+    promise.then((reponse)=> {
+      setUser(reponse.email)
+    }, (err)=> { console.log(err)})
+  },[])
+
+  // function async create user signup for AppWrite
+  const createUser =  async () =>{
+    const client = new Client();
+    const account = new Account(client);
+
+    client
+    .setEndpoint(process.env.NEXT_PUBLIC_ENDPOINT!)
+    .setProject(process.env.NEXT_PUBLIC_PROJECT!)
+    
+    const response = account.create(
+      'grace', 'birindwan@gmail.com', 'firene123#'
+    )
+    response.then((response)=>{
+      console.log("current",response.email)
+      setUser(response.email)
+    }, (err)=> console.log(err))
+  }
+
+
+  // logout session
+  const logoutSession =  async () =>{
+    const client = new Client();
+    const account = new Account(client);
+
+    client
+    .setEndpoint(process.env.NEXT_PUBLIC_ENDPOINT!)
+    .setProject(process.env.NEXT_PUBLIC_PROJECT!)
+    
+    const response = account.deleteSessions()
+    response.then((response)=>{
+      console.log(response)
+      setUser("")
+    }, (err)=> console.log(err))
+  }
+
+  // login session
+  const createEmailSession =  async () =>{
+    const client = new Client();
+    const account = new Account(client);
+
+    client
+    .setEndpoint(process.env.NEXT_PUBLIC_ENDPOINT!)
+    .setProject(process.env.NEXT_PUBLIC_PROJECT!)
+    
+    const response =  account.createEmailSession('birindwan@gmail.com', 'firene123#')
+    response.then((response)=>{
+      console.log("current",response.providerUid)
+      setUser(response.providerUid)
+    }, (err)=> console.log(err))
+  }
+
+  // create new tweet 
+  const createTweet = async() => {
+    const client = new Client();
+
+  client
+    .setEndpoint(process.env.NEXT_PUBLIC_ENDPOINT!)
+    .setProject(process.env.NEXT_PUBLIC_PROJECT!)
+
+  const databases = new Databases(client);
+  
+  const response = databases.createDocument(
+    process.env.NEXT_PUBLIC_DATABASE!,
+    process.env.NEXT_PUBLIC_TWEETS_COLLECTION!,
+    "uniqueID",
+    {text: "this is the second tweet"}
+  );
+  response.then((response)=> {
+    console.log(response)
+  }, (err)=> console.log(err))
+  }
+
   return (
     <main
       className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
     >
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/pages/index.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+      <header>
+        {user && (<p>hello: {user}</p>)}
+        
+      </header>
+     <div className='flex gap-4'>
+      <button className="px-4 py-2 bg-blue-300 text-red-400" onClick={createUser}>Sign up</button>
+      <button className="px-4 py-2 bg-blue-300 text-red-400" onClick={createEmailSession}>Sign in</button>
+      <button className="px-4 py-2 bg-blue-300 text-red-400" onClick={logoutSession}>logout</button>
+     </div>
+     <div>
+      <div>
+        <button onClick={createTweet} className='px-4 py-3 rounded-xl bg-black text-white'>create tweet</button>
+      </div>
+      {tweets.documents.map((tweet, i)=> (
+        <div key={i}>
+          <h3>{tweet.$id}</h3>
+          <h3>{tweet.text}</h3>
+          <h3>{tweet.$createdAt}</h3>
         </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700/10 after:dark:from-sky-900 after:dark:via-[#0141ff]/40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Discover and deploy boilerplate example Next.js&nbsp;projects.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+      ))}
+     </div>
     </main>
   )
 }
@@ -129,14 +134,14 @@ GetServerSideProps<{tweets: Tweet} | Tweet> = async () => {
   const client = new Client();
 
   client
-    .setEndpoint('http://localhost/v1')
-    .setProject('646bcf1ce1b1d65be2d3')
+    .setEndpoint(process.env.NEXT_PUBLIC_ENDPOINT!)
+    .setProject(process.env.NEXT_PUBLIC_PROJECT!)
 
   const databases = new Databases(client);
   
   const tweets = await databases.listDocuments(
-    '646bd1ee75edc0e5e0d8',
-    '646bd206909bd29bc4f1'
+    process.env.NEXT_PUBLIC_DATABASE!,
+    process.env.NEXT_PUBLIC_TWEETS_COLLECTION!
   );
 
   return {
